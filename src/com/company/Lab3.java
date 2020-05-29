73eee13772e95f6eb8af92cdcd808c7655d14bce
Dmitriy Stavnichuk <globalobjectiveart@gmail.com>
2020-05-29 19:06:16 +0300

    Lab WOrk1 java final version 1.1

---
+++ src/com/company/Lab3.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Lab3 {
+    public static void main(String[] args) {
+        double start = 0;
+        double finish = Math.PI;
+        double deltaX = 0.1;
+        double area = 0;
+        double y = 0.2;
+
+        for (double x = 0; x < finish; x+=deltaX) {
+            double rectangle = 0;
+            if(Math.sin(x) < y) {
+                rectangle = Math.sin(x) * deltaX;
+            }
+            else {
+                rectangle = y *deltaX;
+            }
+            area += rectangle;
+        }
+        System.out.println(area);
+    }
+}
