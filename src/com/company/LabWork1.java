81cc35204f253ca890b04e4d2f5aea201e654161
Dmitriy Stavnichuk <globalobjectiveart@gmail.com>
2020-05-31 16:06:14 +0300

    Lab WOrk1 java final version 1.2

--- src/com/company/LabWork1.java
+++ src/com/company/LabWork1.java
@@ -1,14 +1,12 @@
 /*
  * Lab Work 1, Harry Potter book
  *
- * Version info 1
+ * Version info 1.1
  *
  * Copyright Stavnichuk Dmytro
 
  */
 package com.company;
-
-import org.w3c.dom.ls.LSOutput;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -21,6 +19,7 @@ public class LabWork1 {
 
         String text = new String(Files.readAllBytes(Paths.get(
                 "E:\\IdeaProjects\\1_module\\src\\com\\company\\Harry.txt")));
+        String HashWords = "";
         String longword = "";
         String CommonWords = "";
         String[] keyword = {"Harry"};
@@ -34,9 +33,12 @@ public class LabWork1 {
                 .replaceAll("\\?", "")
                 .toLowerCase()
                 .replaceAll("\\.", "");
-        String[] words = clearedText.split(" ");
-        int LineCount = 0;
 
+        String[] words = clearedText.split(" ");
+        String[] lines = text.split("\n");
+        Integer [] HashCodes = new Integer[text.length()];
+
+        int LineCount = 0;
         for (int i = 0; i < words.length; i++) {
             if (words[i].length() >= longword.length()) {
                 longword = words[i];
@@ -44,30 +46,42 @@ public class LabWork1 {
         }
         System.out.println("The longest word in this text is " + longword);
         System.out.println("It contains "+longword.length()+ " letters");
-        System.out.println(words.hashCode());
-        for (int i = 0; i < words.length; i++) {
-            System.out.println(words[i].hashCode());
-        }
+//        for (int i = 0; i < words.length; i++) {
+//            array = (words[i].hashCode());
+//        }
         int count = 0;
         int count2;
-        for (int i = 0; i < words.length; i++) {
-            if (words[i].contains("harry"))
-                count++;
+        int count3;
+        int HarryLines =0;
+        for (int i = 0; i < lines.length; i++) {
+            if (lines[i].contains("Harry"))
+                HarryLines++;
         }
-        System.out.println("Word Harry" + " found in " + count + " lines");
-        System.out.println("Duplicate Characters are:");
+        System.out.println("Word Harry contains in " + HarryLines + " lines");
+        System.out.println("Duplicate Hashwords are:");
 
         for (int i = 0; i < words.length; i++) {
             count2 = 1;
+            count3 = 1;
             for (int j = i + 1; j < words.length; j++) {
                 if (words[i].equals(words[j])) {
                     count2++;
-                    words[j] = "0";
+                    words[j]= "0";
                 }
             }
-            if (count2 > 1 && words[i] != "0")
-                System.out.println("The word: "+ words[i]+ " repeats "+count2+
+
+                HashCodes[i] = words[i].hashCode();
+            for (int j1 = i+1; j1 < words.length ; j1++) {
+                if(HashCodes[i].equals(HashCodes[j1]))
+                {
+                    count3++;
+                    HashCodes[j1] = 0;
+                }
+            }
+            if (count3 > 1 && HashCodes[i] != 0){
+                System.out.println("The word: "+ HashCodes[i] + " repeats "+count3+
                         " times ");
+        }
         }
     }
 }
